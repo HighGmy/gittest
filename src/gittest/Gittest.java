d036e117d5551b04361333c317019d8bb28df5ab
Wongsakorn <sahatat44@gmail.com>
2020-10-20 17:08:24 +0700

    new method

--- src/gittest/Gittest.java
+++ src/gittest/Gittest.java
@@ -4,9 +4,19 @@ public class Gittest {
 
     public static void main(String[] args) {
         sayHello();
+        sayGoodbye();
+        sayGoodmorning();
     }
 
-    public static void sayHello(){
+    public static void sayHello() {
         System.out.println("Hello");
+    }
+
+    public static void sayGoodbye() {
+        System.out.println("Goodbye");
+    }
+
+    public static void sayGoodmorning() {
+        System.out.println("Goodmorning");
     }
 }
